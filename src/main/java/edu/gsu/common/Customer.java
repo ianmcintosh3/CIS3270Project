18cb5e645c845cf20bc499bbe65bc6fe3f12816a
anhphan <anhnguyenphan03@gmail.com>
2024-11-21 14:02:11 -0500

    phan2 commit8

--- src/main/java/edu/gsu/common/Customer.java
+++ src/main/java/edu/gsu/common/Customer.java
@@ -1,4 +1,16 @@
 package edu.gsu.common;
 
 public class Customer {
+
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int zipcode;
+    private String state;
+    private String username;
+    private String password;
+    private String email;
+    private int SSN;
+
 }
