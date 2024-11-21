cdec88e301c5cefe0bf866e0cd2c304e3555fb5a
anhphan <anhnguyenphan03@gmail.com>
2024-11-21 14:10:49 -0500

    phan2 commit9

--- src/main/java/edu/gsu/common/Admin.java
+++ src/main/java/edu/gsu/common/Admin.java
@@ -1,4 +1,8 @@
 package edu.gsu.common;
 
 public class Admin {
+    private int adminId;
+    private String adminName;
+    private String adminUsername;
+    private String adminPassword;
 }
--- src/main/java/edu/gsu/common/Flight.java
+++ src/main/java/edu/gsu/common/Flight.java
@@ -1,4 +1,12 @@
 package edu.gsu.common;
 
 public class Flight {
+
+    private String flightNumber;
+    private String flightDepartureCity;
+    private String flightDestinationCity;
+    private String flightDate;
+    private String flightDepartureTime;
+    private String flightArrivalTime;
+
 }
