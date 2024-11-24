83dfd8c2122da0e9f8f5302652efbb25a5ec6924
Ian McIntosh <ianamcintosh108@gmail.com>
2024-11-24 17:48:49 -0500

    McIntosh7

--- src/main/java/edu/gsu/common/Flight.java
+++ src/main/java/edu/gsu/common/Flight.java
@@ -9,4 +9,41 @@ public class Flight {
     private String flightDepartureTime;
     private String flightArrivalTime;
 
+
+    public void setFlightNumber(String fn) {
+        this.flightNumber = flightNumber;
+    }
+    public String getFlightNumber(String fn){
+        return fn;
+    }
+    public void setFlightDepartureCity(String fdc){
+        this.flightDepartureCity = flightDepartureCity;
+    }
+    public String getFlightDepartureCity(String fdc){
+        return fdc ;
+    }
+    public void setFlightDestinationCity(String fdc){
+        this.flightDestinationCity = flightDestinationCity;
+    }
+    public String getflightDestinationCity(String fdc){
+        return fdc ;
+    }
+    public void setFlightDate(String fd){
+        this.flightDate = flightDate;
+    }
+    public String getFlightDate(String fd){
+        return fd;
+    }
+    public void flightDepartureTime(String fdt){
+        this.flightDepartureTime = flightDepartureTime;
+    }
+    public String getFlightDepartureTime(String fdt){
+        return fdt;
+    }
+    public void setFlightArrivalTime(String fat){
+        this.flightArrivalTime = flightArrivalTime;
+    }
+    public String getFlightArrivalTime(String fat){
+        return fat;
+    }
 }
