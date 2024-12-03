58649cd2bfa1d47f8fcc5491c2d2b9c66ec4880a
anhphan <anhnguyenphan03@gmail.com>
2024-12-02 20:15:10 -0500

    phan2 commit12

--- src/main/java/edu/gsu/gui/LoginPage.java
+++ src/main/java/edu/gsu/gui/LoginPage.java
@@ -6,8 +6,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import edu.gsu.common.Customer;
 import edu.gsu.common.Admin;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
 
 
 public abstract class LoginPage extends Application {
+
 }
